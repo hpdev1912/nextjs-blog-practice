e8d542e9bb303b87a5385b85c4285e179a4d2d21
hpdev1912 <haopham.dev@gmail.com>
2023-02-22 22:57:48 +0700

    refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import Header from "@/layouts/header";
-import { getSortedPostsData } from "@/lib/post";
 import Content from "@/layouts/content";
-import Footer from "@/layouts/footer";
+import { getSortedPostsData } from "@/lib/post";
 import Main from "@/templates/main";
 
 export default function Home({ posts }) {
